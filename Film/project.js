fded41117dc02bf5e5bbaae76a530b1431b9dae9
aslibeyhan <asli.beyhann@gmail.com>
2024-01-03 17:46:04 +0300

    Film Project with ES6 After

--- Film/project.js
+++ Film/project.js
@@ -6,11 +6,7 @@ const urlElement = document.querySelector("#url");
 const cardBody=document.querySelectorAll(".card-body")[1];
 const clear=document.getElementById("clear-films");
 
-//UI objesini başlatma
-const ui=new UI();
-
-//Storage objesi üret
-const storage=new Storage();
+ //UI,STORAGE classları static olduğundan newlemeye gerek yok.Direkt sınıf ismiyle çağırabilirsin.
 
 //Tüm eventleri yükleme
 eventListeners();
@@ -18,8 +14,8 @@ eventListeners();
 function eventListeners(){
    form.addEventListener("submit",addFilm);
    document.addEventListener("DOMContentLoaded",function(){
-    let films=storage.getFilmsFromStorage();
-    ui.loadAllFilms(films);
+    let films=Storage.getFilmsFromStorage();
+    UI.loadAllFilms(films);
    });
 
    cardBody.addEventListener("click",deleteFilm);
@@ -32,35 +28,35 @@ function addFilm(e){
     const url=urlElement.value;
     if(title===""|| director===""||url===""){
         //hata
-        ui.displayMessage("Tüm alanları doldurun","danger");
+        UI.displayMessage("Tüm alanları doldurun","danger");
     }
     else{
         //Yeni film
         const newFilm=new Film(title,director,url);
         //Arayüze film ekleme
-        ui.addFilmToUI(newFilm);
+        UI.addFilmToUI(newFilm);
         //Storage film ekleme
-        storage.addFilmToStorage(newFilm);
-        ui.displayMessage("Film barıyla eklendi","success")
+        Storage.addFilmToStorage(newFilm);
+        UI.displayMessage("Film barıyla eklendi","success")
     }
      
-      ui.clearInputs(titleElement,directorElement,urlElement);
+      UI.clearInputs(titleElement,directorElement,urlElement);
     e.preventDefault();
 }
 
 
 function deleteFilm(e){
     if(e.target.id==="delete-film"){
-        ui.deleteFilmFromUI(e.target);
-        storage.deleteFilmFromStorage(e.target.parentElement.previousElementSibling.previousElementSibling.textContent);
-        ui.displayMessage("Silme islemi basarili","success");
+        UI.deleteFilmFromUI(e.target);
+        Storage.deleteFilmFromStorage(e.target.parentElement.previousElementSibling.previousElementSibling.textContent);
+        UI.displayMessage("Silme islemi basarili","success");
     }
 }
 
 function clearAllFilms(){
     if(confirm("Emin misiniz ?")){
-        ui.clearAllFilmsFromUI();
-        storage.clearAllFilmsFromStorage();
+        UI.clearAllFilmsFromUI();
+        Storage.clearAllFilmsFromStorage();
     }
  
 }
